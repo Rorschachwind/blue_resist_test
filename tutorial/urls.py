9af4213a0bcb5694bc5218c147a4872dd9aebb10
xiaopeng0828@gmail.com <xiaopeng0828@gmail.com>
2018-03-28 15:17:49 -0400

    add a tutorial folder

---
+++ tutorial/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^your-custom-query/', views.get_custom_query),
+    url(r'^your-selection/', views.get_selection),
+    url(r'^your-query/', views.test_query),
+    url(r'^map/', views.get_map),
+]
+
+# change get_query to test_query
