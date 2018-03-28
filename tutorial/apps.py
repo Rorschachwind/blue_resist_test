38108a8acefa1ab738abb302cc192fbbfe29f3e0
Rorschach <34044700+Rorschachwind@users.noreply.github.com>
2018-03-28 15:31:33 -0400

    class tutorialConfig

--- tutorial/apps.py
+++ tutorial/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class AnalysisConfig(AppConfig):
-    name = 'analysis'
+class TutorialConfig(AppConfig):
+    name = 'tutorial'
