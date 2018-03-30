3aa72dda83665e5f8fa8f58a838ad0ab3dbd713f
xiaopeng0828@gmail.com <xiaopeng0828@gmail.com>
2018-03-30 11:51:01 -0400

    add new html

--- tutorial/views.py
+++ tutorial/views.py
@@ -3,7 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+import inspect
+from django.db import connection
 # Create your views here.
 
 def index(request):
     return render(request,'tutorial/index.html')
+
+def about(request):
+    return render(request,'tutorial/about.html')
+
+def learn(request):
+    return render(request,'tutorial/learn.html')
+
+def contribute(request):
+    return render(request,'tutorial/contribute.html')
+
